5613dcc8f3011e0782a708ad653a3d759460d678
jzse <jzse@users.noreply.github.com>
2017-04-12 16:57:10 +1000

    feat: Pass all leap tests

---
+++ ecmascript/leap/leap.js
@@ -0,0 +1,14 @@
+function isLeapYear(year) {
+  if (year % 4 === 0) {
+    if (year % 100 === 0) {
+      if (year % 400 === 0) {
+        return true;
+      }
+      return false;
+    }
+    return true;
+  }
+  return false;
+}
+
+export default isLeapYear;
