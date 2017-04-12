f5a4151f8ac75272ec70052ab1475e62f66b4ab4
jzse <jzse@users.noreply.github.com>
2017-04-13 01:05:41 +1000

    feat: Pass all bob tests

--- ecmascript/bob/bob.js
+++ ecmascript/bob/bob.js
@@ -1,15 +1,17 @@
-//
-// This is only a SKELETON file for the 'Bob' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 class Bob {
   hey(message) {
-    //
-    // YOUR CODE GOES HERE
-    //
+    // Minimum of 2 uppercase chars and ends '!' OR all uppercase chars.
+    if ((message.endsWith('!') && /[A-Z]{2}/.test(message)) || /^[A-Z][A-Z\d\W]+$/.test(message)) {
+      return 'Whoa, chill out!';
+      // Ends with '?'
+    } else if (message.endsWith('?')) {
+      return 'Sure.';
+      // Empty message or contains just spaces.
+    } else if (!message || /^[\s]*$/.test(message)) {
+      return 'Fine. Be that way!';
+    }
+    return 'Whatever.';
   }
 }
 
 export default Bob;
-
