46ff5c79e27c8fb14a0bc90e1001c8739e5923c3
jzse <jzse@users.noreply.github.com>
2017-04-12 17:00:32 +1000

    refactor: Simplify leap

--- ecmascript/leap/leap.js
+++ ecmascript/leap/leap.js
@@ -1,14 +1,5 @@
 function isLeapYear(year) {
-  if (year % 4 === 0) {
-    if (year % 100 === 0) {
-      if (year % 400 === 0) {
-        return true;
-      }
-      return false;
-    }
-    return true;
-  }
-  return false;
+  return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
 }
 
 export default isLeapYear;
